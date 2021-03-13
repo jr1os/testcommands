64557b3ecba7dae4b57b0d61c4f0d2266f42219d
Ali Rios <ali.rios@gmail.com>
2021-03-12 22:40:21 -0300

    Create app flask

---
+++ views.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+def init_app(app: Flask):
+    @app.route("/")
+    def index():
+        return "<h1>Hola Flask</h1>"
+
+    @app.route("/about/<string:nome>/")
+    def about(nome):
+        return f'My name is {nome}'
